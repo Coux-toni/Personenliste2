a854645ca75b3faed926ae78330085e97bf24fff
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2023-01-20 12:38:43 +0100

    Projektdateien hinzufügen.

---
+++ Person.cs
@@ -0,0 +1,59 @@
+﻿using Personenliste2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personenliste2
+{
+    public class Person
+    {
+        private string _vorname = "";
+
+        public string Vorname
+        {
+            get { return _vorname; }
+            set
+            {
+                if (_vorname != value)
+                {
+                    _vorname = value;
+                }
+            }
+        }
+        private string _nachname = "";
+
+        public string Nachname
+        {
+            get { return _nachname; }
+            set
+            {
+               _nachname = value;
+            }
+        }
+        private string _geschlecht = "";
+
+        public string Geschlecht
+        {
+            get { return _geschlecht; }
+
+            set
+            {
+                if (value == "m" || value == "w")
+                {
+                    _geschlecht = value;
+                }
+            }
+        }
+
+
+        public Person(string vorname, string nachname, string geschlecht)
+        {
+            this.Nachname = nachname;
+            this.Vorname = vorname;
+            this.Geschlecht = geschlecht;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Personenliste2;
+Person p1 = new Person("Arian", "Lackner", "m");
+Person p2 = new Person("Can", "Kayac", "m");
+Person p3 = new Person("Fabian", "Eder", "m");
+Console.WriteLine("{0} {1} {2}",p1.Vorname, p1.Nachname, p1.Geschlecht);
+Console.WriteLine("{0} {1} {2}",p2.Vorname, p2.Nachname, p2.Geschlecht);
+Console.WriteLine("{0} {1} {2}",p3.Vorname, p3.Nachname, p3.Geschlecht);
