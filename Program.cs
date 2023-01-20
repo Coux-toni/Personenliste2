05ea3491bed1ed2066fbe008622b4e1661334008
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2023-01-20 13:00:22 +0100

    Persons added to list

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿using Personenliste2;
+using System.ComponentModel;
+
 Person p1 = new Person("Arian", "Lackner", "m");
 Person p2 = new Person("Can", "Kayac", "m");
 Person p3 = new Person("Fabian", "Eder", "m");
-Console.WriteLine("{0} {1} {2}",p1.Vorname, p1.Nachname, p1.Geschlecht);
-Console.WriteLine("{0} {1} {2}",p2.Vorname, p2.Nachname, p2.Geschlecht);
-Console.WriteLine("{0} {1} {2}",p3.Vorname, p3.Nachname, p3.Geschlecht);
+List<Person> Personen = new List<Person> { };
+
+Personen.Add(p1.Vorname,p1.Nachname,p1.Geschlecht);
+Personen.Add(p2.Vorname,p2.Nachname,p2.Geschlecht);
+Personen.Add(p3.Vorname,p3.Nachname,p3.Geschlecht);
+
+
+for(int i = 0; i >= Personen.Count; i++)
+{
+    if (Personen[i].Geschlecht == "m")
+    {
+        Console.WriteLine(Personen);
+    }
+}
