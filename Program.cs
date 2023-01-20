c80819c61a062cf6f3692f7ee3a37519f74a0cbe
Antonio Govorusic <agovorusic@schueler.hakzell.at>
2023-01-20 13:02:48 +0100

    Persons added to list right way

--- Program.cs
+++ Program.cs
@@ -6,11 +6,9 @@ Person p2 = new Person("Can", "Kayac", "m");
 Person p3 = new Person("Fabian", "Eder", "m");
 List<Person> Personen = new List<Person> { };
 
-Personen.Add(p1.Vorname,p1.Nachname,p1.Geschlecht);
-Personen.Add(p2.Vorname,p2.Nachname,p2.Geschlecht);
-Personen.Add(p3.Vorname,p3.Nachname,p3.Geschlecht);
-
-
+Personen.Add(p1);
+Personen.Add(p2);
+Personen.Add(p3);
 for(int i = 0; i >= Personen.Count; i++)
 {
     if (Personen[i].Geschlecht == "m")
